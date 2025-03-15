9055f3041f7051dda968a672d7877fe45bda55a0
manelarfi <manelarfi@gmail.com>
2025-03-15 22:16:04 +0100

    donation routes finished

--- backend/controllers/donationController.js
+++ backend/controllers/donationController.js
@@ -1,3 +1,52 @@
 const asyncHandler = require("express-async-handler");
 const { gateway, network, contract } = require('../blockchain/network/networkConnect');
 
+const getReceiverDonations = asyncHandler(async (req, res) => {
+    try {
+        const { receiverName } = req.params;
+
+        const donations = await contract.evaluateTransaction("getReceiverDonations", receiverName);
+
+        res.status(200).json({ donations: JSON.parse(donations.toString()) });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+const getDonorDonations = asyncHandler(async (req, res) => {
+    try {
+        const { donorID } = req.params;
+
+        const donations = await contract.evaluateTransaction("getDonorDonations", donorID);
+
+        res.status(200).json({ donations: JSON.parse(donations.toString()) });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+const getDonationByID = asyncHandler(async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const donation = await contract.evaluateTransaction("getDonationByID", id);
+
+        res.status(200).json({ donation: JSON.parse(donation.toString()) });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+const getCampaignDonations = asyncHandler(async (req, res) => {
+    try {
+        const { campaignId } = req.params;
+
+        const donations = await contract.evaluateTransaction("getCampaignDonations", campaignId);
+
+        res.status(200).json({ donations: JSON.parse(donations.toString()) });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = { getReceiverDonations, getDonorDonations, getDonationByID, getCampaignDonations };
