bf3eb73d1a2b6c283d6407b0b7f444e8679fe214
Naaylla <nailawissame.rahmoune@etu.usthb.dz>
2025-03-15 16:16:45 +0100

    fixed paths

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -9,10 +9,10 @@ const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> }, 
   {
-    path: "/app",
+    path: "/",
     element: <Layout />,
     children: [
-      { path: "/", element: <Home /> }, 
+      { path: "", element: <Home /> }, 
       { path: "/donate", element: <Donate /> },
     ],
   },
