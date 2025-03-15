ff2add4be8017c498ddcef2b88865f7ba0b88358
lmn69exe <bensalah.ibtihal2005@gmail.com>
2025-03-15 08:10:54 +0100

    added the navigation bar

--- frontend/src/components/Hero/index.tsx
+++ frontend/src/components/Hero/index.tsx
@@ -1,5 +1,14 @@
 export default function Hero() {
-    return (
-        <div>Hero</div>
-    )
+  return (
+    <div className="hero">
+      <div className="text">
+        <p>9oufa</p>
+        <p>Your Gatway To Volunteering</p>
+      </div>
+
+      <div className="images">
+        <img src="" alt="" className="image" />
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/Layout/Header/index.tsx
+++ frontend/src/components/Layout/Header/index.tsx
@@ -1,5 +1,21 @@
+import logo from "../../../assets/header/logo.svg";
+import login_button from "../../../assets/header/login_button.svg";
+
 export default function Header() {
-    return (
-        <div>Header</div>
-    )
+  return (
+    <div className="flex items-center justify-between p-4 bg-[#FEF9E1]">
+      <img src={logo} alt="Logo" className="h-10" />
+      <ul className="flex gap-[83px] font-medium">
+        <li>Donate</li>
+        <li>Funds</li>
+        <li>About</li>
+        <li>Sponsors</li>
+        <li>Contact</li>
+      </ul>
+
+      <button className="flex items center ">
+        <img src={login_button} alt="" />
+      </button>
+    </div>
+  );
 }
