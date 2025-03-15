436f3ca7d648df590f11626878a5a719bb79b454
manelarfi <manelarfi@gmail.com>
2025-03-15 07:25:09 +0100

    user registration

--- backend/config/dbConection.js
+++ backend/config/dbConection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try
     {
+        console.log("MongoDB URI:", process.env.CONNECTION_STRING);
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log(
             "Database connected: ",
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const dotenv = require("dotenv");
 const connectDB = require("./config/dbConection");
+const dotenv = require("dotenv").config();
 const errorHandler = require('./middleware/errorHandler');
 
+connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/users", require("./routes/userRoute"));
 app.use(errorHandler);
 
